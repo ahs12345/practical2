d4ef77ab3a7e5d95c0d3c1d5682cc732d7825563
ahs12345 <75584185+ahs12345@users.noreply.github.com>
2022-03-12 22:36:33 +1030

    Add files via upload

---
+++ Human.h
@@ -0,0 +1,9 @@
+#ifndef HUMAN_H
+#define HUMAN_H
+class Human{
+    public:
+    Human();
+    char makeMove();  
+
+};
+#endif
---
+++ Referee.h
@@ -0,0 +1,11 @@
+#ifndef REFEREE_H
+#define REFEREE_H
+#include "Computer.h"
+#include "Human.h"
+
+class Referee{
+    public:
+    Referee();
+    char refGame(Human player1, Computer player2)  ;
+};
+#endif
